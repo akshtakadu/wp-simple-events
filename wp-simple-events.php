dc7454fcc988e41be0f9ede376d7bd8edcb97242
Akshata Sampat Kadu <139231569+akshtakadu@users.noreply.github.com>
2026-01-17 12:41:36 +0530

    Register Event custom post type

--- wp-simple-events.php
+++ wp-simple-events.php
@@ -10,3 +10,37 @@
  * Text Domain: wp-simple-events
  */
 defined('ABSPATH') || exit;
+// Register Custom Post Type: Event
+function wse_register_event_cpt() {
+
+    $labels = array(
+        'name'                  => 'Events',
+        'singular_name'         => 'Event',
+        'menu_name'             => 'Events',
+        'name_admin_bar'        => 'Event',
+        'add_new'               => 'Add New',
+        'add_new_item'          => 'Add New Event',
+        'new_item'              => 'New Event',
+        'edit_item'             => 'Edit Event',
+        'view_item'             => 'View Event',
+        'all_items'             => 'All Events',
+        'search_items'          => 'Search Events',
+        'not_found'             => 'No events found',
+        'not_found_in_trash'    => 'No events found in Trash',
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'menu_icon'          => 'dashicons-calendar',
+        'supports'           => array('title', 'editor'),
+        'has_archive'        => true,
+        'rewrite'            => array('slug' => 'events'),
+        'show_in_rest'       => true,
+    );
+
+    register_post_type('event', $args);
+}
+
+add_action('init', 'wse_register_event_cpt');
+
