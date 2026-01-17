eb8600d8b685242a00a16b35e4af5e412126b2a1
Akshata Sampat Kadu <139231569+akshtakadu@users.noreply.github.com>
2026-01-17 12:33:46 +0530

    Add initial plugin file for WP Simple Events
    Initial commit of the WP Simple Events plugin with metadata.

---
+++ wp-simple-events.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Simple Events
+ * Plugin URI: https://github.com/akshtakadu/wp-simple-events
+ * Description: A simple WordPress plugin to manage and display events.
+ * Version: 1.0.0
+ * Author: Akshata Sampat Kadu
+ * Author URI: https://github.com/akshtakadu
+ * License: GPLv2 or later
+ * Text Domain: wp-simple-events
+ */
+
+defined('ABSPATH') || exit;
